9aa0fa41082fc33780c5bdc07d6ea28a09818058
idemirtas28 <idemirtas28@my.whitworth.edu>
2025-09-09 23:22:14 -0700

    feat: add placeholder text for current events in EventsPage component

--- app/events/page.js
+++ app/events/page.js
@@ -5,11 +5,17 @@ export default function EventsPage() {
   return (
     <main>
       <section className="hidden md:grid justify-center mt-16 mx-10 grid-cols-4 gap-4">
+        <p className="col-span-4 text-center text-lg">
+          Current events are template, data is not live yet.
+        </p>
         {events.map((event, i) => (
           <EventCard key={i} {...event} />
         ))}
       </section>
       <section className="grid md:hidden justify-center mt-16 mx-10 grid-cols-1 gap-4">
+        <p className="col-span-1 text-center text-lg">
+          Current events are template, data is not live yet.
+        </p>
         {events.map((event, i) => (
           <EventCard key={i} {...event} />
         ))}
